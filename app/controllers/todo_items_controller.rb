80780e8eef1bf810cb3780e7f8562b5263e9c5e2
Jmingus <jmingus@austin.rr.com>
2015-01-27 12:15:15 -0600

    Adding gems and app.js

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,32 +1,22 @@
 class TodoItemsController < ApplicationController
   def index
     @todos = TodoItem.all
-    @todo = TodoItem.new
-  end
-
-  def new
+    @todo  = TodoItem.new
   end
 
   def create
     @todo = TodoItem.new(todo_items_params)
     if @todo.save
       respond_to do |format|
-        format.xhr {render "Success!" }
-        format.html { redirect_to todo_items_path, notice: 'Successfully Created'}
+        format.json { render json: @todo, status: :created, message: "Success!" }
+        format.html { redirect_to todo_items_path, notice: 'Successfully Created!' }
       end
-
     else
       respond_to do |format|
-        format.xhr {render "Error" }
-        format.html {redirect_to :back, alert: 'error occured'}
+        format.json { render json: @todo.errors, status: :unprocessable_entity, message: "Error" }
+        format.html { redirect_to :back, alert: 'error occured' }
       end
     end
-  end
-
-  def show
-  end
-
-  def destroy
   end
 
   private
